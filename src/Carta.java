bd4061b03d5f126a7d8eaca091dc6653227e390c
Star Power <dayan.marulanda@correounivalle.edu.co>
2026-03-23 15:37:57 -0500

    Creación de la clase abstracta Carta

---
+++ src/Carta.java
@@ -0,0 +1,16 @@
+public abstract class Carta {
+
+    protected String nombre;
+
+    public Carta(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
