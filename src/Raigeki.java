3079ecb8acee9496187c9fc5ed38c3b3f797b637
unknown <juan.alejandro.marquez@correounivalle.edu.co>
2026-04-04 11:53:33 -0500

    Se me calma que apenas estoy aprendiendo

---
+++ src/Raigeki.java
@@ -0,0 +1,12 @@
+public class Raigeki extends Magica{
+    public Raigeki(){
+        super("Raigeki");
+    }
+
+    @Override
+    public void activar(Jugador jugador, Jugador oponente){
+        oponente.getCampo().clear();
+    }
+    
+}
+
