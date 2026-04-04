def7da19226fd8e7a4d2eb7f95d034ade569ebe1
unknown <juan.alejandro.marquez@correounivalle.edu.co>
2026-04-04 11:43:35 -0500

    La otra vez lo subi con restored y no cambio

--- src/BoostAtk.java
+++ src/BoostAtk.java
@@ -9,6 +9,7 @@ public class BoostAtk extends Magica{
         if(!jugador.getCampo().isEmpty()){
             Monstruo m = jugador.getCampo().get(0);
             System.out.println("ATK de" + m.getNombre() + "aumento de 500");
+            m.setAtk(m.getAtk() + 500);
         } else {
             System.out.println("No tienes monstruos para aumentar su ataque");
         }
