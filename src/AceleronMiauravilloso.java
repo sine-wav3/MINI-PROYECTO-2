5c9680a48d8e0636ebb8b13c8c1ec42ca049e7b8
unknown <juan.alejandro.marquez@correounivalle.edu.co>
2026-04-04 11:50:37 -0500

    Miau

---
+++ src/AceleronMiauravilloso.java
@@ -0,0 +1,18 @@
+public class AceleronMiauravilloso extends Magica{
+
+    public AceleronMiauravilloso(){
+        super("Aceleron Miauravilloso");
+    }
+
+    @Override
+    public void activar(Jugador jugador, Jugador oponente){
+        if(!jugador.getCampo().isEmpty()){
+            Monstruo m = jugador.getCampo().get(0);
+            System.out.println("DEF de" + m.getNombre() + "aumento de 200");
+            m.setDef(m.getDef() + 200);
+        } else {
+            System.out.println("No tienes monstruos para aumentar su defensa");
+        }
+    }
+    
+}
